bb8c12a170e078cefeffbca112f656745de1d44c
samson-mutangili <samsonkimole@gmail.com>
2023-10-23 12:38:23 +0300

    Added a method to delete a topic

--- src/main/java/com/samson/courseapi/topic/TopicService.java
+++ src/main/java/com/samson/courseapi/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.samson.courseapi.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("Spring", "Spring Framework", "Spring framework description"),
 			new Topic("Java", "Core Java", "Core Java description"),
 			new Topic("Javascript", "JavaScript", "Javascript description")
-			);
+			));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -24,5 +25,22 @@ public class TopicService {
 
 	public void addTopic(Topic topic) {
 		topics.add(topic);		
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		
+		for(int i=0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			
+			//update if id equals to id in the parameter
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+			}
+		}
+		
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
 	}
 }
